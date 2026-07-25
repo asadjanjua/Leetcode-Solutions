1aa1f5fb1bd519b35e833580f99efb15dc9b2824
Asad Mehmood <143835996+asadjanjua@users.noreply.github.com>
2026-07-25 09:02:15 +0500

    Time: 0 ms (100%), Space: 18 MB (39.78%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+
         unordered_map<int,int> freq;
-        for (int i=0;i<nums.size();i++){
+
+        // Count frequency
+        // [1,1,1,2,2,3] -> {1:3,2:2,3:1}
+        for(int i=0;i<nums.size();i++)
             freq[nums[i]]++;
-        }
+
+        // Store (number,frequency)
+        // (1,3) (2,2) (3,1)
         vector<pair<int,int>> arr;
-        for(auto i=freq.begin();i!=freq.end();i++){
+        for(auto i=freq.begin();i!=freq.end();i++)
             arr.push_back({i->first,i->second});
-        }
-        sort(arr.begin(),arr.end(),[](pair<int,int>&a,pair<int,int>&b)
-        {
+
+        // Sort by frequency (highest first)
+        // (1,3) (2,2) (3,1)
+        sort(arr.begin(),arr.end(),[](pair<int,int>& a,pair<int,int>& b){
             return a.second>b.second;
         });
-        vector <int> ans;
-            for(int i=0;i<k;i++){
-                ans.push_back(arr[i].first);
-            }
-            return ans;
+
+        vector<int> ans;
+
+        // Take first k numbers
+        // k=2 -> [1,2]
+        for(int i=0;i<k;i++)
+            ans.push_back(arr[i].first);
+
+        return ans;
     }
 };
